9c3bdb9b88ae62112204c640618389818330f619
gururajmudakannavar17-lang <mudakannavargururaj@gmail.com>
2026-07-20 18:40:54 +0530

    Merge remote changes

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const menu = document.querySelector('.menu');
-const dropdown = document.querySelectorAll('.dropdown');
-
-menu.addEventListener('click',function(){
-    menu.parentElement.classList.toggle('open');
-    document.body.classList.toggle('nav-open');
-})
-dropdown.forEach(function(item){
-    item.addEventListener('click',function(){
-        item.parentElement.classList.toggle('link-open');
-    })
-})
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
